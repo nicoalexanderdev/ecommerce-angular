d33c0be271f78d93c9eb2ede9cc9e813645b9462
nicoalexanderdev <nicoalexanderdev@gmail.com>
2024-10-02 22:15:47 -0300

    clean components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
 import { CartComponent } from './cart/cart.component';
-import { CategoriasComponent } from './categorias/categorias.component';
-import { MarcasComponent } from './marcas/marcas.component';
 import { SearchComponent } from './search/search.component';
 import { ProductComponent } from './product/product.component';
 
@@ -10,8 +8,6 @@ export const routes: Routes = [
     { path: '', redirectTo: '/tienda', pathMatch: 'full' },
     { path: 'tienda', component: ProductListComponent },
     { path: 'tienda/cart', component: CartComponent },
-    { path: 'tienda/categorias', component: CategoriasComponent },
-    { path: 'tienda/marcas', component: MarcasComponent },
     { path: 'tienda/search', component: SearchComponent },
     { path: 'tienda/product/:id/:nombre', component: ProductComponent }
 ];
